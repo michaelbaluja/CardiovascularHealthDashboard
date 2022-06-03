b10a552bc9f533564bd9baa21d3724bed37fb5bc
Uday Varikuti <udaykumar0496@gmail.com>
2022-06-03 04:53:03 -0700

    Modified layout for the main page. Added a short description

--- src/app/app_new.py
+++ src/app/app_new.py
@@ -1,10 +1,7 @@
-from importlib.resources import path
 from dash import Dash, dcc, html, Input, Output, callback
 import dash_bootstrap_components as dbc
 import dash_extensions as de
 from pages import page0,page1, page2, page3,page4
-
-
 
 app = Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
@@ -17,20 +14,24 @@ app.layout = html.Div([
     html.Div(id='page-content')
 ])
 
+description = "This dashboard aims to provide users with information about their current heart health condition with a personalized \
+    risk assessment for users, keeping their privacy in mind. It provide visualizations analyzing the relationship between \
+    several risk factors and cardiovascular diseases"
+
 index_page =html.Div(children=[
                     #title
-                    html.Div(html.H1("Heart Disease Risk Prediction and Risk Factor Analysis",style={'margin-left': '12%','opacity':'70%'}),style={'background': 'rgb(0,255,156)','opacity':'38%'}),
+                    html.Div(className="app-header", children=[html.H1("Cardio Care")]),
+                    #style={'margin-left': '12%','opacity':'70%'}),style={'background': 'rgb(0,255,156)','opacity':'38%',
 
                     #description
-                    html.Div(html.H2("Description"),style={'width': '30%',' height':' 43%','margin-top': '6%','margin-left': '3%','background': 'rgb(0,255,156)','opacity':'38%'}),
+                    html.Div(html.H5(description),style={'width': '30%',' height':' 43%','margin-top': '6%','margin-left': '10%'}),
 
-
-
+                    html.Br(),
                     html.Div(
                     children=[  
                         html.Div(style={'left':'67%', 'width':' 37%'},className="HeartBeat",children=[
                         html.Div(de.Lottie(options=options, url=url)),
-                        dcc.Link(html.Button("Get Started",style={    'width': '44%','margin-left': '31%'}), href="/page0", refresh=True),
+                        dcc.Link(html.Button("Get Started",style={'width': '44%','margin-left': '31%'}), href="/page0", refresh=True),
                 ])
 ])
 ])
