3d55246a447b5e771e7eb8eb83c0a97cc502f215
adeshpande3003 <100742642+adeshpande3003@users.noreply.github.com>
2022-06-02 18:19:12 -0700

    Update kaggle_compute_corr.py

--- src/app/kaggle_compute_corr.py
+++ src/app/kaggle_compute_corr.py
@@ -4,24 +4,30 @@ from sklearn.feature_selection import SelectKBest, chi2
 # from dash import Dash, html, dcc, Input, Output
 # import plotly.express as px
 
-CATEGORICAL_COLUMNS = ['gender', 'cholesterol', 'smoke', 'alcohol', 'active']
+CATEGORICAL_COLUMNS = ['gender', 'High cholesterol', 'smoke', 'alco', 'active','High glucose']
 
 def get_categorical_table()-> pd.DataFrame:
     """Returns a section of the table with categorical variables only
-
     Returns
     -------
     pandas.DataFrame
     """
     # read the kaggle dataset
-    df = pd.read_csv('../data/kaggle_cleaned.csv')
-    return df[[*CATEGORICAL_COLUMNS, 'cardio']]
+    df = pd.read_csv('../kaggle_cleaned.csv')
+    dummies = pd.get_dummies(df['cholesterol'],prefix='cholesterol')
+    res = pd.concat([df, dummies], axis=1)
+    res = res.drop(['cholesterol','cholesterol_1','cholesterol_3'],axis=1)
+    res = res.rename(columns = {"cholesterol_2":"High cholesterol"})
+    dummies = pd.get_dummies(df['gluc'],prefix='gluc')
+    res = pd.concat([res, dummies], axis=1)
+    res = res.drop(['gluc','gluc_1','gluc_3'], axis=1)
+    res = res.rename(columns = {"gluc_2":"High glucose"})
+    return res[[*CATEGORICAL_COLUMNS, 'cardio']]
 
 df = get_categorical_table()
 
 def get_dropdown_options():
     """Returns all the varibles to populate the dropdown
-
     Returns
     -------
     list(str)
@@ -30,12 +36,10 @@ def get_dropdown_options():
 
 def compute_importance(x_vars) -> list:
     """Computes the importance score using the chi-squared statistic
-
     Input
     -------
     x_vars : list of fields for which the importance is computed
     y : Dependent variable
-
     Returns
     -------
     list: importance scores for each x with respect to y
@@ -73,8 +77,8 @@ def compute_importance(x_vars) -> list:
 # def correlation_plot(cols):
 #     """Correlation between the selected factors and cardiovascular disease risk.
 #     """
-#     return px.bar(x=cols, y=compute_importance(cols), title='Correlation',
-#         labels={'x': 'Risk Factors', 'y':'Correlation'}, width=500)
+#     return px.bar(x=cols, y=compute_importance(cols), title='Correlation Plot For Risk Factors',
+#                   labels={'x': 'Risk Factors', 'y':'Correlation'}, width=700)
 
 # if __name__ == '__main__':
 #     app.run_server(debug=True)
