c850767d568e857a3ed66c987fa2eddc1166c009
Uday Varikuti <udaykumar0496@gmail.com>
2022-06-03 02:31:04 -0700

    Updated map plot + fixed aggregation bug in the bar plot (location based plot)

--- src/app/cdc_plots.py
+++ src/app/cdc_plots.py
@@ -3,12 +3,12 @@ import plotly.express as px
 from utils.validation import _zip_to_county
 from uszipcode import SearchEngine
 
-DF_PERCENT_FILEPATH =  '../../data/trends_by_percent.csv'
+# DF_PERCENT_FILEPATH =  '../../data/trends_by_percent.csv'
 DF_100K_FILEPATH = '../../data/trends_by_100k.csv'
 TRENDS_DF_PATH = '../../data/trends_heart_disease.csv'
 HOSPITAL_DF_PATH = '../../data/us_hospital_locations.csv'
 
-df_percent = pd.read_csv(DF_PERCENT_FILEPATH)
+# df_percent = pd.read_csv(DF_PERCENT_FILEPATH)
 df_100k = pd.read_csv(DF_100K_FILEPATH)
 trends_df = pd.read_csv(TRENDS_DF_PATH)
 hosp_df = pd.read_csv(HOSPITAL_DF_PATH)
@@ -39,7 +39,7 @@ def get_age_statistics(zip: str):
     county_name = _zip_to_county(zip)
     if county_name.rsplit(' ',1)[-1] == 'County':
         county_name = county_name.rsplit(' ', 1)[0]
-    county_df = df_100k[df_100k['County'] == county_name]
+    county_df = df_100k[df_100k['County'] == county_name].groupby(['Age', 'Topic']).sum().reset_index()
     fig = px.bar(county_df, x="Age", color="Topic", 
                 y='Data_Value',
                 title=f"Number of Moratilies in {county_name} County (per 100k) from 1999-2018 (combined)",
@@ -84,18 +84,14 @@ def get_hospital_data(zip: str):
     -------
     (px scatter mapbox :  map plot)
     """
-    filtered_df = hosp_df[hosp_df['ZIP'] == zip]
     engine = SearchEngine()
-    # lat_str = filtered_df['LATITUDE'].iloc[0]
-    # lng_str = filtered_df['LONGITUDE'].iloc[0]
     zip_obj = engine.by_zipcode(int(zip))
     lat_str = zip_obj.lat
     lng_str = zip_obj.lng
     center_dict = {'lat': lat_str, 'lon':lng_str}
 
     fig = px.scatter_mapbox(hosp_df, lat="LATITUDE", lon="LONGITUDE", hover_name="NAME", hover_data=["ADDRESS", "WEBSITE"],
-                            color_discrete_sequence=["light blue"], title="Map of Nearby Hospitals",
-                            zoom=12, height=400, center=center_dict)
+                            color_discrete_sequence=["light blue"], zoom=12, center=center_dict)
     fig.update_layout(mapbox_style="open-street-map")
     fig.update_traces(marker_size=15)
     fig.update_layout(margin={"r":1,"t":1,"l":1,"b":1})
