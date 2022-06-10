539ed0a7d6dfc98c7afd97ba011883c964992439
Michael Baluja <michaelbaluja@protonmail.com>
2022-06-09 22:11:12 -0700

    Add app tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,31 @@
 from dash import html
 
+from src.app import data
+
 from .. import app
 
 
-def test_display_page0():
-    page = app.display_page('/page0')
-    assert isinstance(page, html.Div)
+class TestDisplayPages:
+    def test_page0(self):
+        page = app.display_page('/page0')
+        assert isinstance(page, html.Div)
+
+    def test_page1(self):
+        page = app.display_page('/page1')
+        assert isinstance(page, html.Div)
+
+    def test_page2(self):
+        page = app.display_page('/page2')
+        assert isinstance(page, html.Div)
+
+    def test_page3(self):
+        page = app.display_page('/page3')
+        assert isinstance(page, html.Div)
+
+    def test_page4(self):
+        page = app.display_page('/page4')
+        assert isinstance(page, html.Div)
+
+    def test_index(self):
+        page = app.display_page(None)
+        assert isinstance(page, html.Div)
